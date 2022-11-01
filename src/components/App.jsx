684a04ff2164fe6a05346b58ad75103cdf9788c7
Leon7422 <y.kurduban98@gmail.com>
2022-11-01 13:45:05 +0200

    add localstorage logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,22 @@ import { ContactList } from './ContactList/ContactList';
 
 export class Phonebook extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    if (JSON.parse(localStorage.getItem('contacts'))) {
+      const contacts = JSON.parse(localStorage.getItem('contacts'));
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(pP, pS) {
+    if (this.state.contacts !== pS.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   filterInputChange = e => {
     this.setState({ filter: e.currentTarget.value });
